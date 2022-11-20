5a6d646b10427a1a3d623363ce40977ee7e639b2
elsayedz <elsayed.z@aucegypt.edu>
2022-11-20 22:25:18 +0200

    Working async server

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,139 @@
 mod thread_pool;
-use thread_pool::ThreadPool;
+use httparse::Header;
 
+use std::collections::HashMap;
+use std::env;
 use std::io::prelude::*;
+use std::net::SocketAddr;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 use std::fs;
+use std::sync::Arc;
+
 use std::thread;
 use std::time::Duration;
 
 use httparse;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4).unwrap();
-       
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+use tokio::task;
+// use tokio::sync::Mutex;
+use futures::lock::Mutex;
+
+struct ServersInfo {
+    id_to_ip: Arc<Mutex<HashMap<String, String>>>,
+    pub my_id : Arc<Mutex<String>>,
+}
+
+impl ServersInfo {
+    fn new(_ip1:String, _ip2:String, _ip3:String, _my_id:String) -> Self {
+
+        let mut ip_id: HashMap<String, String> = HashMap::new();
         
-         pool.execute(|| {
-            handle_connection(stream);
-        });
+        ip_id.insert("0".to_string(), _ip1.clone());
+        ip_id.insert("1".to_string(), _ip2.clone());
+        ip_id.insert("2".to_string(), _ip3.clone());
+    
+        println!("------------Constructor-----------");
+        for (key, value) in &ip_id {
+            println!("Server id: {} --> IP {}", key, value);
+        }
+        
+        Self {
+            id_to_ip: Arc::new(Mutex::new(ip_id)),
+            my_id: Arc::new(Mutex::new(_my_id)),
+        }
+    }
+}
+struct Server {
+    servers_info: ServersInfo
+}
+
+impl Server {
+    fn new(_ip1:String, _ip2:String, _ip3:String, _my_id:String) -> Self {
+        Self {
+            servers_info: ServersInfo::new(_ip1, _ip2, _ip3, _my_id)
+        }
+    }
+
+    async fn init_election(&self, headers: & mut [Header<'_>]) {
+        println!("Init election called");
+        let mut id = String::new();
+        for header in headers {
+            if header.name == "id" {
+                id = String::from_utf8(header.value.to_vec()).unwrap();
+            }
+        }
+        println!("id: {}", id);
+
+        let mut min_id = std::i32::MAX;
+        let map = self.servers_info.id_to_ip.lock().await;
+
+        for (key, _value) in &*map {
+            if key.parse::<i32>().unwrap() < min_id {
+                min_id = key.parse::<i32>().unwrap();
+            }
+        }
+        println!("min_id: {}", min_id);
+    }
+
+    async fn force_failure(&self){
+        println!("Force failure called");
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+
+
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let ip_server1 = &args[1];
+    let ip_server2 = &args[2];
+    let ip_server3 = &args[3];
+    
+    let main_server_index = &args[4].parse::<usize>().unwrap();
+    
+    let servers = vec![ip_server1, ip_server2, ip_server3];
+    let mut ips: Vec<String> = Vec::new();
+    for i in 0..servers.len() {
+        if i != *main_server_index {
+            ips.push(servers[i].to_string());
+        }
+    }
+    let server_addr:SocketAddr = servers[*main_server_index].parse().unwrap();
+    let ip1 = format!("{}{}","http://" ,servers[0]);
+    let ip2 = format!("{}{}","http://" ,servers[1]);
+    let ip3 = format!("{}{}","http://" ,servers[2]);
+    let my_id = format!("{}", main_server_index.to_string());
+    println!("Main server listening on: {}", servers[*main_server_index]);
+    println!("Server2 listening on {}", ip2);
+    println!("Server3 listening on {}", ip3);
+
+    // let _servers_info = ServersInfo::new(ip1, ip2, ip3, main_server_index.to_string());
+    let ip1_ref = Arc::new(Mutex::new(ip1));
+    let ip2_ref = Arc::new(Mutex::new(ip2));
+    let ip3_ref = Arc::new(Mutex::new(ip3));
+    let my_id_ref = Arc::new(Mutex::new(my_id));
+    let listener = TcpListener::bind(server_addr).unwrap();
+    
+    
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        let ip1_ref = Arc::clone(&ip1_ref);
+        let ip2_ref = Arc::clone(&ip2_ref);
+        let ip3_ref = Arc::clone(&ip3_ref);
+        let my_id_ref = Arc::clone(&my_id_ref);
+        let join = task::spawn(async{
+            handle_connection(stream, ip1_ref, ip2_ref, ip3_ref, my_id_ref).await;
+        });
+
+    }
+}
+
+
+async fn handle_connection(mut stream: TcpStream, ip1: Arc<Mutex<String>>, ip2:Arc<Mutex<String>>, ip3:Arc<Mutex<String>>, my_id:Arc<Mutex<String>>) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
@@ -33,8 +142,37 @@ fn handle_connection(mut stream: TcpStream) {
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = httparse::Request::new(&mut headers);
     let res = req.parse(&buffer).unwrap();
+
     println!("res: {:?}", res);
+    println!("req: {:?}", req);
     
+    let mut fn_name: String = String::new();
+    for header in req.headers[..].iter() {
+        if header.name == "fn" {
+            fn_name = std::str::from_utf8(header.value).unwrap().to_string();
+            println!("fn_name: {}", fn_name);
+        }
+    }
+    let ip1 = ip1.lock().await;
+    let ip2 = ip2.lock().await;
+    let ip3 = ip3.lock().await;
+    let my_id = my_id.lock().await;
+
+    let server =Server::new(ip1.to_string(), ip2.to_string(), ip3.to_string(), my_id.to_string());
+    match fn_name.as_str() {
+        "init_election" => {
+            server.init_election(req.headers).await;
+        },
+        "force_failure" => {
+            server.force_failure().await;
+        },
+        _ => {
+            println!("No function found");
+        }
+    }
+        
+    
+
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
@@ -53,4 +191,5 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
 }
